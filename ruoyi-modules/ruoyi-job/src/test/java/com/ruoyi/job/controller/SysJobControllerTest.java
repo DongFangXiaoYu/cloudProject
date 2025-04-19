9b6dc6ea90645d39f7e1625dce52a2d8b865e29c
shukai <1054602781@qq.com>
2025-04-19 20:39:48 +0800

    20250417--单元测试

---
+++ ruoyi-modules/ruoyi-job/src/test/java/com/ruoyi/job/controller/SysJobControllerTest.java
@@ -0,0 +1,17 @@
+package com.ruoyi.job.controller;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @description
+ * @AUTHER: sk
+ * @DATE: 2025/4/19
+
+**/
+
+class SysJobControllerTest {
+
+    @Test
+    void list() {
+    }
+}
