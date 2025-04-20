1ce4c8c550736b4059a17ff5664c6def467ef737
shukai <1054602781@qq.com>
2025-04-20 23:05:45 +0800

    20250420--单元测试

---
+++ ruoyi-modules/ruoyi-system/src/test/java/com/ruoyi/system/controller/SysConfigControllerTest.java
@@ -0,0 +1,18 @@
+package com.ruoyi.system.controller;
+
+import org.junit.Test;
+
+
+
+/**
+ * @description
+ * @AUTHER: sk
+ * @DATE: 2025/4/20
+ **/
+
+public class SysConfigControllerTest {
+
+    @Test
+    public void list() {
+    }
+}
