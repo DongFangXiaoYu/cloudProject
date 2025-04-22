9d809eebd80ca51f0bbd76aa62010b50b86a80b4
shukai <1054602781@qq.com>
2025-04-22 16:19:27 +0800

    20250422--测试

---
+++ ruoyi-modules/ruoyi-system/src/test/java/com/ruoyi/system/controller/SysOperlogControllerTest.java
@@ -0,0 +1,18 @@
+package com.ruoyi.system.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @description
+ * @AUTHER: sk
+ * @DATE: 2025/4/22
+ **/
+
+public class SysOperlogControllerTest {
+
+    @Test
+    public void list() {
+    }
+}
