b6c135bdebc7aefaa85302dd8d973164c1a1bbc5
shukai <1054602781@qq.com>
2025-04-23 22:22:36 +0800

    20250423--测试

---
+++ ruoyi-modules/ruoyi-gen/src/test/java/com/ruoyi/gen/controller/GenControllerTest.java
@@ -0,0 +1,21 @@
+package com.ruoyi.gen.controller;
+
+import org.junit.Test;
+
+/**
+ * @description
+ * @AUTHER: sk
+ * @DATE: 2025/4/23
+
+**/
+
+public class GenControllerTest {
+
+    @Test
+    public void genList() {
+    }
+
+    @Test
+    public void getInfo() {
+    }
+}
