3553104a4c34d5a4764461c16e2c895b95f914fe
shukai <1054602781@qq.com>
2025-04-23 22:16:06 +0800

    20250422--测试

---
+++ ruoyi-modules/ruoyi-system/src/test/java/com/ruoyi/system/controller/SysPostControllerTest.java
@@ -0,0 +1,22 @@
+package com.ruoyi.system.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @description
+ * @AUTHER: sk
+ * @DATE: 2025/4/23
+ **/
+
+public class SysPostControllerTest {
+
+    @Test
+    public void list() {
+    }
+
+    @Test
+    public void export() {
+    }
+}
