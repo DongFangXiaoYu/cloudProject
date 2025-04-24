911694ee5692703969afc1c4c27d3bf4f6aa1dad
shukai <1054602781@qq.com>
2025-04-24 22:02:51 +0800

    20250423--测试

---
+++ ruoyi-modules/ruoyi-system/src/test/java/com/ruoyi/system/controller/SysLogininforControllerTest.java
@@ -0,0 +1,22 @@
+package com.ruoyi.system.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @description
+ * @AUTHER: sk
+ * @DATE: 2025/4/24
+ **/
+
+public class SysLogininforControllerTest {
+
+    @Test
+    public void list() {
+    }
+
+    @Test
+    public void export() {
+    }
+}
