99da00d537f9b4bb7d0c411b488fb4668db1de9c
shukai <1054602781@qq.com>
2025-04-25 20:38:30 +0800

    20250425--测试

---
+++ ruoyi-modules/ruoyi-system/src/test/java/com/ruoyi/system/controller/SysNoticeControllerTest.java
@@ -0,0 +1,34 @@
+package com.ruoyi.system.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @description
+ * @AUTHER: sk
+ * @DATE: 2025/4/25
+ **/
+
+public class SysNoticeControllerTest {
+
+    @Test
+    public void list() {
+    }
+
+    @Test
+    public void getInfo() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void edit() {
+    }
+
+    @Test
+    public void remove() {
+    }
+}
