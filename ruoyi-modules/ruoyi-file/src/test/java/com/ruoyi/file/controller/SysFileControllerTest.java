6a19e81a0df3425526e62036fd7e551fe763e04a
shukai <1054602781@qq.com>
2025-04-27 20:46:14 +0800

    20250427--测试

---
+++ ruoyi-modules/ruoyi-file/src/test/java/com/ruoyi/file/controller/SysFileControllerTest.java
@@ -0,0 +1,17 @@
+package com.ruoyi.file.controller;
+
+import org.junit.Test;
+
+/**
+ * @description
+ * @AUTHER: sk
+ * @DATE: 2025/4/27
+
+**/
+
+public class SysFileControllerTest {
+
+    @Test
+    public void upload() {
+    }
+}
